6a24164c99b2178014345e33ab3f522774f23fcc
Kron Austrum <huyach@protonmail.ch>
2019-04-25 08:16:33 +0300

    add page - uech

---
+++ pages/uech.vue
@@ -0,0 +1,87 @@
+<template lang="pug">
+.uech-page
+  iframe(
+    width='853'
+    height='480'
+    src='https://www.youtube.com/embed/BJwcDrI0pow'
+    frameborder='0'
+    allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+    allowfullscreen=''
+    )
+
+  iframe(
+    width='853'
+    height='480'
+    src='https://www.youtube.com/embed/LVEgXvfKd9Y'
+    frameborder='0'
+    allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+    allowfullscreen=''
+    )
+
+  iframe(
+    width='853'
+    height='480'
+    src='https://www.youtube.com/embed/lSw1ZGJ-Sds'
+    frameborder='0'
+    allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+    allowfullscreen=''
+    )
+
+  iframe(
+    width='853'
+    height='480'
+    src='https://www.youtube.com/embed/FgmR8sdtDhM'
+    frameborder='0'
+    allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+    allowfullscreen=''
+    )
+
+  iframe(
+    width='853'
+    height='480'
+    src='https://www.youtube.com/embed/q2-Xliv9RUw'
+    frameborder='0'
+    allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+    allowfullscreen=''
+    )
+
+  iframe(
+    width='853'
+    height='480'
+    src='https://www.youtube.com/embed/ESllIiI712k'
+    frameborder='0'
+    allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+    allowfullscreen=''
+    )
+
+  iframe(
+    width='853'
+    height='480'
+    src='https://www.youtube.com/embed/hQ_FAnZ-4-c'
+    frameborder='0'
+    allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+    allowfullscreen=''
+    )
+
+
+</template>
+<script lang="coffee">
+export default
+  # props: ['']
+  # components: {}
+  name: 'uech-page'
+  head: ->
+    title: 'Unreal Engine 4'
+  # data: ->
+  # methods:
+  # computed:
+  # watch:
+</script>
+<style lang="stylus" scoped>
+.uech-page
+  gcc()
+  hw(100)
+  background black
+  iframe
+    margin 4em
+</style>
